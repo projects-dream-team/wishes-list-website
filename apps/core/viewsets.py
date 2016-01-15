c02845bb710830b4be7dcc8752d510d0e126cb58
Jacek Rybak <jacek.rybak@keycode.pl>
2016-01-15 19:55:52 +0100

    added missing crf token

--- apps/core/viewsets.py
+++ apps/core/viewsets.py
@@ -2,10 +2,7 @@
 from __future__ import unicode_literals
 from rest_framework import viewsets, status
 from rest_framework.mixins import CreateModelMixin
-from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import *
-from .models import *
 from .serializers import *
 
 
